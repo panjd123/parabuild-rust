d9841a3751c779f990492c539445907b5db5d783
panjd123 <1747366367@qq.com>
2024-11-19 19:14:14 +0800

    Fix error when nvidia-smi is not installed

--- src/cuda_utils.rs
+++ src/cuda_utils.rs
@@ -1,12 +1,15 @@
 use regex::Regex;
 use std::process::Command;
 
-pub fn get_cuda_device_uuids()-> Vec<String> {
-    let output = Command::new("nvidia-smi")
-        .arg("-L")
-        .output()
-        .expect("failed to execute nvidia-smi");
-    let output = String::from_utf8(output.stdout).unwrap();
-    let re = Regex::new(r"\(UUID: (MIG-[a-z0-9\-]+)\)").unwrap();
-    re.captures_iter(&output).map(|cap| cap[1].to_string()).collect()
+pub fn get_cuda_device_uuids() -> Vec<String> {
+    match Command::new("nvidia-smi").arg("-L").output() {
+        Ok(output) => {
+            let output = String::from_utf8(output.stdout).unwrap();
+            let re = Regex::new(r"\(UUID: (MIG-[a-z0-9\-]+)\)").unwrap();
+            re.captures_iter(&output)
+                .map(|cap| cap[1].to_string())
+                .collect()
+        }
+        Err(_) => Vec::new(),
+    }
 }
